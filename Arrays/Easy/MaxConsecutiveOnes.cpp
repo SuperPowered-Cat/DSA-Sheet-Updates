46a4ec5dabebbc07fd5b971ae00e25908f4621e0
Om Thakur <91081968+SuperPowered-Cat@users.noreply.github.com>
2024-08-25 19:32:12 +0530

    Create MaxConsecutiveOnes.cpp

---
+++ Arrays/Easy/MaxConsecutiveOnes.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int count = 0, max_count = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == 1) count++;
+            else {
+                if (max_count<=count) swap(max_count, count);
+                count = 0;
+            }
+        }
+        if (max_count<count) return count;
+        return max_count;
+    }
+};

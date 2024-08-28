c9c83242a662f9aa072cd68937b749afaaa35d58
Om Thakur <91081968+SuperPowered-Cat@users.noreply.github.com>
2024-08-28 12:13:51 +0530

    Update MaximumSubarray.cpp

--- Arrays/Medium/MaximumSubarray.cpp
+++ Arrays/Medium/MaximumSubarray.cpp
@@ -9,6 +9,8 @@ public:
             sum+=nums[i];
             if (max < sum) max = sum;
             if (sum < 0) sum = 0;
+            // if (max<0) max = 0 
+            // this is if the question states that empty arrays are also included.
         }
     return max;
     }

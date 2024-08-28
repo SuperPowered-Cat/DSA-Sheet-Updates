f977e1be0d009255e60e68de31ed108465748184
Om Thakur <91081968+SuperPowered-Cat@users.noreply.github.com>
2024-08-28 11:54:50 +0530

    Update MaximumSubarray.cpp

--- Arrays/Medium/MaximumSubarray.cpp
+++ Arrays/Medium/MaximumSubarray.cpp
@@ -12,4 +12,25 @@ public:
         }
     return max;
     }
+
+    // int maxSubArray(vector<int>& nums) {
+    
+    //   // for returning the max array;
+    //   // Kadane algorithm.
+    //     int max = INT_MIN; 
+    //     int sum = 0;
+    //     int start = 0;
+    //     int fstart = -1, intfend = -1;
+    //     for (int i= 0; i<nums.size(); i++) {
+    //         if (sum===0) start = i;
+    //         sum+=nums[i];
+    //         if (max < sum){
+    //             max = sum;
+    //             fstart = start;
+    //             fend = i;
+    //         }
+    //         if (sum < 0) sum = 0;
+    //     }
+    // return max;
+    // }
 };

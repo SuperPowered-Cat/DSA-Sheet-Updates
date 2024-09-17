5f0b99a26128accfc51145d32bf80335fbc405e6
Om Thakur <91081968+SuperPowered-Cat@users.noreply.github.com>
2024-09-17 15:48:12 +0530

    Create UncommonWordsFrom2Sentence.cpp

---
+++ DailyQuestions/UncommonWordsFrom2Sentence.cpp
@@ -0,0 +1,45 @@
+// leetcode 884
+
+// **Q: Uncommon Words from Two Sentences
+
+// A sentence is a string of single-space separated words where each word consists only of lowercase letters.
+// A word is uncommon if it appears exactly once in one of the sentences, and does not appear in the other sentence.
+// Given two sentences s1 and s2, return a list of all the uncommon words. You may return the answer in any order.
+
+// Example 1:
+// Input: s1 = "this apple is sweet", s2 = "this apple is sour"
+// Output: ["sweet","sour"]
+// Explanation:
+// The word "sweet" appears only in s1, while the word "sour" appears only in s2.
+
+// Example 2:
+// Input: s1 = "apple apple", s2 = "banana"
+// Output: ["banana"]
+
+// Constraints:
+// 1 <= s1.length, s2.length <= 200
+// s1 and s2 consist of lowercase English letters and spaces.
+// s1 and s2 do not have leading or trailing spaces.
+// All the words in s1 and s2 are separated by a single space.
+
+
+
+// CODE ->
+
+class Solution {
+public:
+    int countWords(vector<string>& words1, vector<string>& words2) {
+        unordered_map<string, int> m1;
+        unordered_map<string, int> m2;
+
+        for (auto ch : words1) 
+            m1[ch]++;
+        for (auto ch : words2) 
+            m2[ch]++;
+        int count = 0;
+        for (const auto& pair : m1) {
+            if (pair.second == 1 && m2[pair.first] == 1) count++;
+        }
+        return count;
+    }
+};
